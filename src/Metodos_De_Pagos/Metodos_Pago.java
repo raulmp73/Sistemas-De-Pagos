ff9efc1f44a4dae4f5c3d2c7db8fe1c3eb93ce35
aleejandroofm <ferrandizmartineza9@gmail.com>
2026-01-22 13:01:45 +0100

    Clase Abstracta Implementada

--- src/Metodos_De_Pagos/Metodos_Pago.java
+++ src/Metodos_De_Pagos/Metodos_Pago.java
@@ -1,5 +1,12 @@
 package Metodos_De_Pagos;
 
-public class Metodos_Pago {
-
+public abstract class Metodos_Pago {
+	
+	protected float importe;
+	
+	public Metodos_Pago(float importe) {
+		this.importe = importe;
+	}
+	
+	public abstract void pagar();
 }
